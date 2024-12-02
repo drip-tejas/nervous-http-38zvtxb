73d7f188fceafddc520a471866e1acde52bd2f65
Tejas Aithal <167405474+drip-tejas@users.noreply.github.com>
2024-12-02 16:28:26 +0000

    Analytics Controller and routes added

--- src/routes/qrRoutes.ts
+++ src/routes/qrRoutes.ts
@@ -5,11 +5,13 @@ import {
   getQRCode,
   scanQRCode,
 } from "../controllers/qrController";
+import { getQRCodeAnalytics } from "../controllers/analyticsController";
 const router = express.Router();
 
 router.post("/generate", generateQRCode);
 router.get("/track/:identifier", trackQRCodeScan);
 router.get("/:id", getQRCode);
 router.get("/scan/:id", scanQRCode);
+router.get("/analytics/:id", getQRCodeAnalytics);
 
 export default router;
